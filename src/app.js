b21a645770883ca26b2441ebd50079aa304b4175
rgranatodutra <r.granatodutra@gmail.com>
2023-05-10 14:30:11 -0300

    junio route

--- src/app.js
+++ src/app.js
@@ -93,6 +93,46 @@ app.get("/webhook/renan", (req, res) => {
     };
 });
 
+// Junio Routes
+app.post("/webhook/junio", (req, res) => {
+    try {
+        if(!req.body.entry[0]?.changes[0].value?.messages) {
+
+            return;
+        } else {
+            console.log(new Date().toLocaleString(), ": Received new message.");
+
+            const baseURL = process.env.RENAN_BASEURL || "http://localhost:8000";
+            const api = axios.create({
+                baseURL: baseURL,
+                timeout: 10000
+            });
+    
+            api.post('/whatsapp/message', req.body);
+        
+            return res.status(200).send();
+        };
+    } catch(err){
+        console.log(err);
+    };
+});
+
+app.get("/webhook/junio", (req, res) => {
+    const verify_token = "inpulse";
+
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (mode && token) {
+        if (mode === "subscribe" && token === verify_token) {
+            res.status(200).send(challenge);
+        } else {
+            res.status(403).send();
+        };
+    };
+});
+
 app.listen(7000, () => {
     console.log(new Date().toLocaleString(), `: App is running on http://localhost:7000`);
 });
