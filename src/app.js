2b211a3fa23fb1bad35a7bc56297d2385832f4c1
rgranatodutra <r.granatodutra@gmail.com>
2023-05-19 11:02:37 -0300

    premium

--- src/app.js
+++ src/app.js
@@ -133,6 +133,45 @@ app.get("/webhook/junio", (req, res) => {
     };
 });
 
+app.post("/webhook/premium", (req, res) => {
+    try {
+        if(!req.body.entry[0]?.changes[0].value?.messages) {
+
+            return;
+        } else {
+            console.log(new Date().toLocaleString(), ": Received new message.");
+
+            const baseURL = process.env.PREMIUM_BASEURL || "http://localhost:8000";
+            const api = axios.create({
+                baseURL: baseURL,
+                timeout: 10000
+            });
+    
+            api.post('/whatsapp/message', req.body);
+        
+            return res.status(200).send();
+        };
+    } catch(err){
+        console.log(err);
+    };
+});
+
+app.get("/webhook/premium",(req, res) => {
+    const verify_token = "inpulse";
+
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (mode && token) {
+        if (mode === "subscribe" && token === verify_token) {
+            res.status(200).send(challenge);
+        } else {
+            res.status(403).send();
+        };
+    };
+});
+
 app.listen(7000, () => {
     console.log(new Date().toLocaleString(), `: App is running on http://localhost:7000`);
 });
